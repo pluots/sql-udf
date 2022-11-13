001019db66f258145dbcb897a532925c8a54b7da
Trevor Gross <tmgross@umich.edu>
2022-11-12 20:01:33 -0500

    Initial integration tests successful
    Finish sequence integration testing
    
    Update setup functions
    
    Update using Once

--- udf-examples/tests/backend/mod.rs
+++ udf-examples/tests/backend/mod.rs
@@ -1,70 +1,64 @@
 //! Module to run tests when a backend is available
 //!
-//! This module requires docker to be available on the host system, with the
-//! image called `mdb-example-so`. If that is available, run these tests with
-//! `cargo t --features backend`
+//! This module requires a database that matches the description in
+//! `DEFAULT_DATABASE_URI`. If that is available, run these tests with `cargo t
+//! --features backend`
 
 #![cfg(feature = "backend")]
+use std::collections::HashSet;
+use std::env;
+use std::sync::{Mutex, Once};
 
-use std::ffi::{OsStr, OsString};
-use std::os::unix::ffi::OsStrExt;
-use std::process::Command;
-use std::sync::Mutex;
-
+use diesel::dsl::sql;
 use diesel::mysql::MysqlConnection;
+use diesel::prelude::*;
+use diesel::sql_types::Untyped;
 use diesel::Connection;
+use lazy_static::lazy_static;
 
-const NAME: &str = "mdb-example-so";
-const DATABASE_URL: &str = "mysql://root:example@0.0.0.0:12300/db";
+const URI_ENV: &str = "UDF_TEST_BACKEND_URI";
+const DEFAULT_DATABASE_URI: &str = "mysql://root:example@0.0.0.0:12300/udf_tests";
 
-// Uninitialized by default
-static DOCKER_CONTAINER: Mutex<Option<MariaDbContainer>> = Mutex::new(None);
+static INIT: Once = Once::new();
 
-#[derive(Debug)]
-struct MariaDbContainer {
-    hash: OsString,
+lazy_static! {
+    static ref SETUP_STATE: Mutex<HashSet<String>> = Mutex::new(HashSet::new());
 }
 
-impl MariaDbContainer {
-    fn initialize() -> Self {
-        let hash_bytes = Command::new("docker")
-            .args(["run", "--rm", "-it", "-d", "-p", "12300:3306", NAME])
-            .output()
-            .expect("failed to start docker process")
-            .stdout;
-
-        let hash = OsStr::from_bytes(&hash_bytes).to_os_string();
-        eprintln!("started {:?}", hash);
-        Self { hash }
+fn get_database_uri() -> String {
+    match env::var(URI_ENV) {
+        Ok(s) => s,
+        Err(_) => DEFAULT_DATABASE_URI.to_owned(),
     }
 }
 
-impl Drop for MariaDbContainer {
-    fn drop(&mut self) {
-        Command::new("docker").arg("stop").arg(&self.hash);
+/// Ensure the init items have been run
+pub fn get_db_connection(init: &[&str]) -> MysqlConnection {
+    let db_url = get_database_uri();
+
+    INIT.call_once(|| {
+        let mut conn = MysqlConnection::establish(db_url.rsplit_once('/').unwrap().0)
+            .expect("initial connection failed");
+
+        sql::<(Untyped,)>("create or replace database udf_tests")
+            .execute(&mut conn)
+            .expect("could not create databases");
+    });
+
+    let hset = &mut *SETUP_STATE.lock().unwrap();
+    let mut conn = MysqlConnection::establish(&db_url).expect("initial connection failed");
+
+    // Store a list of our init calls so we don't repeat them
+    for stmt in init {
+        if hset.contains(*stmt) {
+            continue;
+        }
+        sql::<(Untyped,)>(stmt)
+            .execute(&mut conn)
+            .expect("could not run setup");
+
+        hset.insert((*stmt).to_owned());
     }
-}
 
-fn make_connection() -> MysqlConnection {
-    MysqlConnection::establish(DATABASE_URL)
-        .unwrap_or_else(|e| panic!("Error connecting to {}: {e}", DATABASE_URL))
-}
-
-/// Start the container if it hasn't been yet
-pub fn get_db_connection() -> MysqlConnection {
-    let mut guard = DOCKER_CONTAINER.lock().unwrap();
-    // If the container hasn't yet been initialized, do so
-    if guard.is_none() {
-        eprintln!("INITING");
-        *guard = Some(MariaDbContainer::initialize());
-    }
-    eprintln!("{guard:?}");
-    // sleep(std::time::Duration::from_secs(10));
-
-    make_connection()
-}
-
-#[test]
-fn x() {
-    get_db_connection();
+    conn
 }
