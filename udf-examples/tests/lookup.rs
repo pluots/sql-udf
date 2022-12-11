d45705a8e904280971fa09b4496d9b494e960a83
Trevor Gross <tmgross@umich.edu>
2022-12-11 16:55:43 -0500

    Added test to catch #39
    Update buildx context
    
    Updated buffer output text for integration
    
    Bump mariadb version in dockerfile
    
    Wrap buffer-style return types within another struct

--- udf-examples/tests/lookup.rs
+++ udf-examples/tests/lookup.rs
@@ -5,7 +5,7 @@ mod backend;
 use backend::get_db_connection;
 use diesel::dsl::sql;
 use diesel::prelude::*;
-use diesel::sql_types::{Nullable, Text};
+use diesel::sql_types::{Nullable, Text, Untyped};
 
 const SETUP: [&str; 1] = ["create or replace function lookup6
         returns string
@@ -42,4 +42,23 @@ fn test_nonexistant() {
         .expect("bad result");
 
     assert!(res.0.is_none());
+}
+
+#[test]
+fn test_sql_buffer_bug() {
+    // This is intended to catch a buffer problem in mysql/mariadb
+    // See link: https://github.com/pluots/sql-udf/issues/39
+
+    let conn = &mut get_db_connection(&SETUP);
+
+    sql::<(Untyped,)>("set @testval = (select lookup6('0.0.0.0'))")
+        .execute(conn)
+        .unwrap();
+
+    let res: (Option<String>,) =
+        sql::<(Nullable<Text>,)>("select  regexp_replace(@testval,'[:.]','')")
+            .get_result(conn)
+            .expect("bad result");
+
+    assert_eq!(res.0.unwrap(), "ffff0000");
 }
--- udf/src/wrapper/debug.rs
+++ udf/src/wrapper/debug.rs
@@ -3,7 +3,7 @@
 #![cfg(feature = "logging-debug")]
 
 use std::any::type_name;
-use std::ffi::{c_char, c_uchar};
+use std::ffi::{c_char, c_uchar, c_ulong};
 
 use cfg_if::cfg_if;
 use udf_sys::{UDF_ARGS, UDF_INIT};
@@ -32,18 +32,15 @@ pub unsafe fn post_init_call<T>(
     _message: *const c_char,
     ret: bool,
 ) {
-    udf_log!(Debug: "exiting init for `{}`", type_name::<T>());
-
     cfg_if! {
         if  #[cfg(feature = "logging-debug-calls")] {
             udf_log!(Debug: "Data return state at init:");
             dbg!(&*initid);
-            // SAFETY: MYSQL_ERRMSG_SIZE is the length of this buffer
-            // let msg_buf = slice::from_raw_parts(message, MYSQL_ERRMSG_SIZE);
-            // dbg!(&*message);
             eprintln!("Returning {ret:?}");
         }
     }
+
+    udf_log!(Debug: "exiting init for `{}`", type_name::<T>());
 }
 
 pub unsafe fn pre_deinit_call<T>(initid: *const UDF_INIT) {
@@ -74,7 +71,6 @@ pub unsafe fn pre_add_call<T>(
     }
 }
 
-#[allow(dead_code)]
 pub unsafe fn pre_clear_call<T>(initid: *const UDF_INIT, error: *const c_uchar) {
     udf_log!(Debug: "entering clear for `{}`", type_name::<T>());
 
@@ -87,7 +83,6 @@ pub unsafe fn pre_clear_call<T>(initid: *const UDF_INIT, error: *const c_uchar) 
     }
 }
 
-#[allow(dead_code)]
 pub unsafe fn pre_remove_call<T>(
     initid: *const UDF_INIT,
     args: *const UDF_ARGS,
@@ -139,6 +134,54 @@ pub unsafe fn post_process_call<T>(
             dbg!(&*args);
             dbg!(&*is_null);
             dbg!(&*error);
+        }
+    }
+}
+
+pub unsafe fn pre_process_call_buf<T>(
+    initid: *const UDF_INIT,
+    args: *const UDF_ARGS,
+    result: *const c_char,
+    length: *const c_ulong,
+    is_null: *const c_uchar,
+    error: *const c_uchar,
+) {
+    udf_log!(Debug: "entering process for `{}`", type_name::<T>());
+
+    cfg_if! {
+        if  #[cfg(feature = "logging-debug-calls")] {
+            udf_log!(Debug: "Data receive state at process:");
+            dbg!(&*initid);
+            dbg!(&*args);
+            dbg!(result);
+            dbg!(&*length);
+            dbg!(&*is_null);
+            dbg!(&*error);
+        }
+    }
+}
+
+pub unsafe fn post_process_call_buf<T>(
+    initid: *const UDF_INIT,
+    args: *const UDF_ARGS,
+    result: *const c_char,
+    length: *const c_ulong,
+    is_null: *const c_uchar,
+    error: *const c_uchar,
+    ret: *const c_char,
+) {
+    udf_log!(Debug: "exiting process for `{}`", type_name::<T>());
+
+    cfg_if! {
+        if  #[cfg(feature = "logging-debug-calls")] {
+            udf_log!(Debug: "Data return state at process:");
+            dbg!(&*initid);
+            dbg!(&*args);
+            dbg!(result);
+            dbg!(&*length);
+            dbg!(&*is_null);
+            dbg!(&*error);
+            dbg!(ret);
         }
     }
 }
